c9b437a147593e25b5cd28bbc5e06f8b24a9a81d
ohayo <o.hayo@funble.kr>
2022-12-02 18:59:26 +0900

    한데까지 저장

--- rn-style/src/App.js
+++ rn-style/src/App.js
@@ -5,15 +5,32 @@ import { View, Text } from 'react-native';
 
 const Container = styled.View`
   flex: 1;
-  background-color: '#e3e3e3';
+  background-color: '#red';
   align-items: 'center';
   justify-content: 'center';
+`;
+
+const cssText = css`
+  font-size: 20px;
+  font-weight: 600;
+`;
+
+const StyleText = styled.Text`
+  font-size: 20px;
+  font-weight: 600;
+  color: blue;
+`;
+
+const ErrorText = styled.Text`
+  $(cssText)
+  color: :red;
 `;
 
 export default function App() {
   return (
     <Container>
       <StatusBar style="auto" />
+      <StyleText>Styled Components</StyleText>
     </Container>
   );
 }
